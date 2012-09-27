d0060829d2ab6b9f0d2cdb930555381a7b96d938
Andrew Beal <noaddress@whoi.edu>
2012-09-27 21:23:23 +0000

    update file to reflected moved files.
    git-svn-id: https://dev-acomms.whoi.edu/svn/pymodem/trunk@9215 f2731822-534f-144a-aff5-02520857a0fd

--- setup.py
+++ setup.py
@@ -1,47 +1,6 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import shutil
-
-#Examples in SRC directory
-FileList = [
-"bottomnode.py",
-"ctd.py",
-"dispatcher.py",
-"dropbottle.py",
-"ducertest.py",
-"excepttest.py",
-"glider.py",
-"gliderlisten.py",
-"glidertx.py",
-"gwbuoy.py",
-"main.py",
-"spare.py",
-"vfinbottle.py",
-"bottomnode.py",
-"ctd.py",
-"dispatcher.py",
-"dropbottle.py",
-"ducertest.py",
-"excepttest.py",
-"glider.py",
-"gliderlisten.py",
-"glidertx.py",
-"gwbuoy.py",
-"main.py",
-"spare.py",
-"vfinbottle.py"
-]
-
-ScriptList = []
-
-#Move them to the example directory
-for file in FileList:
-	shutil.move("src/%s"%file,"bin/.")
-	ScriptList.append("bin/%d")
-
-print ScriptList
-exit
 
 setup(
     name='MicroModem',
