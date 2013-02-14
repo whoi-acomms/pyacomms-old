e07b30e01b13590a9b76055252f5ab698489e389
Eric Gallimore <egallimore@whoi.edu>
2013-02-14 01:37:43 +0000

    Preparing setup.py for distribution with PyPi.
    git-svn-id: https://dev-acomms.whoi.edu/svn/pyacomms/trunk@9786 f2731822-534f-144a-aff5-02520857a0fd

--- setup.py
+++ setup.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
 from ez_setup import use_setuptools
     
 use_setuptools()
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='acomms',
-    version='1.0',
+    version='0.5b1',
     author='Eric Gallimore, Andrew Beal',
     author_email='pyacomms@whoi.edu',
-    packages=['acomms'],
-	package_dir={'acomms':'acomms'},
-    url='http://acomms.whoi.edu/',
-    license='LICENSE.txt',
-    description='Micromodem Python Tool.',
-    long_description=open('README.txt').read(),
+    packages=['acomms', 'bin', 'examples'],
+    url='http://packages.python.org/acomms/',
+    license='LGPLv3+',
+    description='WHOI Micromodem Interface Library and Tools',
+    long_description=read('README.txt'),
     install_requires=[
         "bitstring >= 3.0.0",
         "pyserial >= 2.6",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering",
     ],
 )
 
