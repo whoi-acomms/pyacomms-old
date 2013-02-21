e8768413ddb6e8afe44d34eedb39343468039ec7
Eric Gallimore <egallimore@whoi.edu>
2013-02-21 08:38:48 +0000

    Fixed some bugs in packedcyclestats byte parsing, glider cst indexing, committing new pyacomms version number.
    git-svn-id: https://dev-acomms.whoi.edu/svn/pyacomms/trunk@9832 f2731822-534f-144a-aff5-02520857a0fd

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='acomms',
-    version='0.5.2b4',
+    version='0.5.2b5',
     author='Eric Gallimore, Andrew Beal',
     author_email='pyacomms@whoi.edu',
     packages=['acomms', 'acomms.modem_connections', 'bin',],
